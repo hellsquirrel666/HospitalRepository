6866482de35f259eb2365e838e601eca1ae89782
lauraPineda <ARELLY_10@HOTMAIL.COM>
2016-06-30 01:03:17 -0500

    .

---
+++ MyHospital/Medicamento/VerMedicamentos.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MyHospital.Modelo;
+
+namespace MyHospital.Medicamento
+{
+    public partial class VerMedicamentos : System.Web.UI.Page
+    {
+        dbHospitalEntities _dataModel = new dbHospitalEntities();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetUsuarios();
+
+        }
+
+        #region "Metodos"
+        private void GetUsuarios()
+        {
+            var query =from m in _dataModel.Medicamentos
+                       select new {m.nIdMedicamento, m.sNombre, m.sLaboratorio};
+
+            var results = query.ToList();
+
+            gvMedicamentos.DataSource = results;
+            gvMedicamentos.DataBind();
+        }
+        #endregion
+    }
+}
