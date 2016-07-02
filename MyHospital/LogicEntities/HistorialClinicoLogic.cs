dcc9ebaa3122e2d8b0e71270327a0525eea746d8
lauraPineda <ARELLY_10@HOTMAIL.COM>
2016-07-02 14:48:53 -0500

    historial clinico

---
+++ MyHospital/LogicEntities/HistorialClinicoLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MyHospital.Modelo;
+
+namespace MyHospital.LogicEntities
+{
+    public class HistorialClinicoLogic
+    {
+        public HitorialClinico ActualizarOGuardarCampo(Modelo.HitorialClinico historial)
+        {
+            try
+            {
+                if (historial == null)
+                {
+                    throw new ArgumentException("No se puede guardar un valor nulo en Paciente.", "Paciente");
+                }
+                using (var db = new dbHospitalEntities())
+                {
+                    db.Entry(historial).State = historial.nIdHistorial == 0 ? EntityState.Added : EntityState.Modified;
+                    db.SaveChanges();
+                    return historial;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Ocurrió un error en el servicio web.", e);
+            }
+        }
+    }
+}
--- MyHospital/Paciente/VerHistorial.aspx.cs
+++ MyHospital/Paciente/VerHistorial.aspx.cs
@@ -5,11 +5,19 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using MyHospital.Modelo;
+using System.Data;
+using MyHospital.LogicEntities;
+
 
 namespace MyHospital.Paciente
 {
+
     public partial class VerHistorial : System.Web.UI.Page
     {
+        int IdCampo;
+        int IdHistorial;
+        string Descripcion;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!this.IsPostBack)
@@ -20,6 +28,22 @@ namespace MyHospital.Paciente
 
         protected void btnGuardar_Click(object sender, EventArgs e)
         {
+            TextBox txtObservaciones;
+            foreach (GridViewRow row in gvCampos.Rows)
+            {
+                txtObservaciones = row.Cells[2].FindControl("txtObservaciones") as TextBox;
+                IdCampo = Convert.ToInt32(row.Cells[0].Text);
+                IdHistorial = string.IsNullOrEmpty(row.Cells[1].Text) ? default(int) : int.Parse(row.Cells[1].Text); ;
+                Descripcion = txtObservaciones.Text;
+
+                if (IdHistorial != 0 || !String.IsNullOrEmpty(Descripcion))
+                {
+                    HistorialClinicoLogic hcl = new HistorialClinicoLogic();
+                    hcl.ActualizarOGuardarCampo(ObtenerDireccion());
+
+                }
+            }
+            InitializeControls();
         }
 
         protected void btnCancelar_Click(object sender, EventArgs e)
@@ -32,18 +56,34 @@ namespace MyHospital.Paciente
             var idPaciente = Request.QueryString["Paciente"];
             using (var _dataModel = new dbHospitalEntities())
             {
+
                 var lista = (from c in _dataModel.CamposHistClin
                                 join hc in _dataModel.HitorialClinico on c.nIdCampoHistClin equals hc.nIdCampoHistClin into gj
                                 from subhc in gj.DefaultIfEmpty()
+                                where c.bActivo==true
                              select new {   c.nIdCampoHistClin, 
                                             c.sDescripcion, 
                                             nIdHistorial = (subhc == null ? 0 : subhc.nIdHistorial),
                                             sObservaciones = (subhc == null ? String.Empty : subhc.sObservaciones) }
                     ).ToList();
-                gvPacientes.DataSource = lista;
-                gvPacientes.DataBind();
+                gvCampos.DataSource = lista;
+                gvCampos.DataBind();
             }
         }
+
+        public HitorialClinico ObtenerDireccion()
+        {
+            HitorialClinico direccion = new HitorialClinico
+            {
+                nIdCampoHistClin=IdCampo,
+                nIdPaciente = int.Parse(Request.QueryString["Paciente"]),
+                nIdHistorial=IdHistorial,
+                sObservaciones = Descripcion
+            };
+            return direccion;
+        }
+
+
 
     }
 }
