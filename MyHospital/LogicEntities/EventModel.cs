c3c443de51702d7f237454327d12497545b0bb50
Hellsquirrel666 <erickcab1989@outlook.com>
2016-07-03 23:59:48 -0500

    Avance Calendar

---
+++ MyHospital/LogicEntities/EventModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyHospital.LogicEntities
+{
+    public class EventModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string @class { get; set; }
+        public string start { get; set; }//Milliseconds
+        public string end { get; set; } // Milliseconds
+
+
+    }
+}
